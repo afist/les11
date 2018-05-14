c09c6d4f4f84eefda21303f37043628bdc0ff390
Afist <barahuda@ua.fm>
2018-05-14 08:53:45 +0300

    first commit

---
+++ src/Processing/CheckCookie.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Инга
+ * Date: 13.05.2018
+ * Time: 18:20
+ */
+
+namespace Socket\Processing;
+
+class CheckCookie
+{
+
+    /**
+     * CheckCookie constructor.
+     */
+    private $arrCookie;
+    private $key;
+    private $testCoockie;
+
+    public function __construct($cookie, $key, $testCoockie)
+    {
+        $this->arrCookie = $cookie;
+        $this->key = $key;
+        $this->testCoockie = $testCoockie;
+
+
+    }
+    public function checkCookie()
+    {
+        $check = false;
+        if ($this->arrCookie[$this->key] === $this->testCoockie ){
+            $check = true;
+        }
+        return $check;
+    }
+
+}
---
+++ src/Processing/keyStorage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Инга
+ * Date: 13.05.2018
+ * Time: 18:04
+ */
+namespace Socket\Processing;
+
+class keyStorage
+{
+
+}
